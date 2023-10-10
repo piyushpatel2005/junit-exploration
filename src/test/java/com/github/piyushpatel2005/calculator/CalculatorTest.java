a784c544e117ebb5de80093b483e0c48c3a20172
Piyush Patel <piyushpatel2005@gmail.com>
2023-10-10 17:12:42 -0400

    Added first unit test

--- src/test/java/com/github/piyushpatel2005/calculator/CalculatorTest.java
+++ src/test/java/com/github/piyushpatel2005/calculator/CalculatorTest.java
@@ -1,7 +1,13 @@
 package com.github.piyushpatel2005.calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CalculatorTest git log{
-
+class CalculatorTest {
+    @Test
+    void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.add(1, 1));
+    }
 }
